9a8a7dd18dcf0a2cec7d76740f96994506bc2e02
Raphael <raphael.garzaner@gmail.com>
2019-11-21 15:54:24 +0100

    Tom stinkt

--- Datastructures/Binary Tree/models/BinaryTree.cs
+++ Datastructures/Binary Tree/models/BinaryTree.cs
@@ -371,7 +371,7 @@ namespace Binary_TreeApp.models
                 //1.Fall:
                 if ((treeItemToRemove.LeftItem == null) && (treeItemToRemove.RightItem != null))
                 {
-                    //raphi stinkt
+                    //fabischiach
                     if (itemBefore.RightItem == treeItemToRemove)
                     {
                         itemBefore.RightItem = treeItemToRemove.RightItem;
